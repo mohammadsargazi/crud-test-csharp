83c727ce6d6730cde4ed77ce6826bbb0ac3f51dc
msargazi <m.sargazi1370@yahoo.com>
2023-06-22 19:35:20 +0330

    Implement customerrepository

---
+++ Infrastructure/Data/Repository/CustomerRepository.cs
@@ -0,0 +1,36 @@
+﻿using Data.Repository.Base;
+using Domain.Entities;
+using Domain.Interfaces;
+using MongoDB.Entities;
+
+namespace Data.Repository;
+
+public class CustomerRepository : BaseRepository<Customer>, ICustomerRepository
+{
+    public async Task<bool> CheckDataExistence(string firstname, string lastname, DateTimeOffset? dateOfBirth)
+    {
+        return await DB.Find<Customer>()
+            .Match(c => c.Firstname == firstname &&
+                         c.Lastname == lastname &&
+                         c.DateOfBirth == dateOfBirth)
+            .ExecuteAnyAsync();
+    }
+
+    public async Task<bool> IsDuplicateCustomer(Customer customer)
+    {
+        return await DB.Find<Customer>()
+            .Match(c => c.Firstname == customer.Firstname &&
+                         c.Lastname == customer.Lastname &&
+                         c.Email == customer.Email &&
+                         c.ID != customer.ID)
+            .ExecuteAnyAsync();
+
+    }
+
+    public async Task<bool> IsExistEmail(string email)
+    {
+        return await DB.Find<Customer>()
+             .Match(c => c.Email == email)
+             .ExecuteAnyAsync();
+    }
+}
